5cb609b4c2cd62924a52d2ab3ec5c02e41c8feb9
Charan P <147381598+charanp2006@users.noreply.github.com>
2025-05-08 14:41:33 +0530

    Add files via upload

---
+++ stockpilot-frontend/src/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          brand: '#FF5733',  // Custom brand color
+        },
+      },
+    },
+  }
+//   module.exports = {
+//     content: [
+//       './src/**/*.{html,js}',
+//       './public/index.html',
+//     ],
+//     theme: {
+//       extend: {},
+//     },
+//     plugins: [],
+//   }
+    
